ab9c9a309957e3aacac028c3071095d4e388979c
Lori Salazar <lorisalazar8@gmail.com>
2019-02-05 22:01:56 -0800

    initial files + CustomizedInputBase component

---
+++ src/components/Resources/Resources.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+import CustomizedInputBase from '../CustomizedInputBase'
+
+const styles = {
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    width: 1,
+    height: 28,
+    margin: 4,
+  },
+};
+
+
+class Resources extends Component {
+  state = {  }
+  render() {
+    return (
+      <CustomizedInputBase/>
+
+    );
+  }
+}
+
+export default Resources;
