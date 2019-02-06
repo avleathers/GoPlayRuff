7268a4b6d6f36129ab3ab71f4576b429d64ca7fb
Missy Leathers <missy@avleathers.com>
2019-02-06 15:58:24 -0800

    Updated UI colors and added a dialog button but it needs to be moved to the AppBar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import About from "./components/About";
 import CreateAccount from "./components/CreateAccount";
-// import Game from "./components/Game";
+import Game from "./components/Game";
 import Resources from "./components/Resources";
 import SignIn from "./components/SignIn";
 import "./App.css";
@@ -11,9 +11,8 @@ import Puzzle from "./components/Game/Game";
 const App = () => (
   <div className="container-fluid mainContainer">
     <About />
-    {/* <CreateAccount />  */}
-    <Puzzle />
-    asd
+    <CreateAccount /> 
+    <Game />
     {/* <Resources /> */}
     {/* <SignIn />    */}
   </div>
