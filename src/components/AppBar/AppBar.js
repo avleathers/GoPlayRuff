7b6c8bc3ddea4d47c1b5e95241fb7826ef864bbf
Missy Leathers <missy@avleathers.com>
2019-02-06 11:49:36 -0800

    Added npm express and morgan files

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -12,6 +12,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = {
   root: {
+    position: sticky,
     flexGrow: 1,
   },
   grow: {
@@ -27,7 +28,7 @@ function ButtonAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="sticky">
         <Toolbar>
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
             <MenuIcon />
