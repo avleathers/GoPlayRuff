7d9a954a07261fa8d7883e1c2eb368587eb61081
Missy Leathers <missy@avleathers.com>
2019-02-11 11:25:54 -0800

    Updating folder structure to include a Client folder

--- routes/user.js
+++ routes/user.js
@@ -7,3 +7,14 @@ module.exports = api => {
 	api.route('/users/').post(User.post);
 	api.route('/users/:userId').delete(User.delete);
 };
+
+app.get("/users", function(req, res) {
+	db.userInfo.find({})
+	.then(function(dbUser) {
+	  res.json(dbUser);
+	})
+	.catch(function(err) {
+	  res.json(err);
+	});
+   });
+   
