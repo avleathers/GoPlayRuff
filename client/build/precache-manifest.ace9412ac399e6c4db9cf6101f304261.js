472faeb79601446f6999430b5cf06959ea2fc2f8
Lori Salazar <lorisalazar8@gmail.com>
2019-02-11 19:15:35 -0800

    Added client/build production files (Taylor request)

---
+++ client/build/precache-manifest.ace9412ac399e6c4db9cf6101f304261.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f4f03e05f976e34806b48f576aae252",
+    "url": "/static/media/image_part_002.7f4f03e0.jpg"
+  },
+  {
+    "revision": "d8a2765886a5f29c5203",
+    "url": "/static/css/main.489aa218.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7e07a5d3d0bb8ee41feb",
+    "url": "/static/js/2.7e07a5d3.chunk.js"
+  },
+  {
+    "revision": "7dff72da32b8a25f77143282a6b26706",
+    "url": "/static/media/MissyRusty.7dff72da.jpg"
+  },
+  {
+    "revision": "c9a09f1b57f9fd26cc2a14e08e177996",
+    "url": "/static/media/LorisLabs.c9a09f1b.jpg"
+  },
+  {
+    "revision": "c045fa2ef30939d442ff16ce3c6ac2f2",
+    "url": "/static/media/ColinWheaten.c045fa2e.jpg"
+  },
+  {
+    "revision": "af1369ca5cf32f4cdd01b0ef7357351c",
+    "url": "/static/media/image_part_001.af1369ca.jpg"
+  },
+  {
+    "revision": "d8a2765886a5f29c5203",
+    "url": "/static/js/main.d8a27658.chunk.js"
+  },
+  {
+    "revision": "a607e76197e756a1d6c835bbdc19cbd2",
+    "url": "/static/media/image_part_003.a607e761.jpg"
+  },
+  {
+    "revision": "f48c2de425d9b22eb5818fefbf7b8c59",
+    "url": "/static/media/image_part_004.f48c2de4.jpg"
+  },
+  {
+    "revision": "faaefc67fe6280677de5327240d1e62d",
+    "url": "/static/media/image_part_005.faaefc67.jpg"
+  },
+  {
+    "revision": "5bd5df3d824b04806755f2e7bdac66e9",
+    "url": "/static/media/image_part_006.5bd5df3d.jpg"
+  },
+  {
+    "revision": "efa8ffffe425a865d66b3fba7b98df5b",
+    "url": "/static/media/image_part_007.efa8ffff.jpg"
+  },
+  {
+    "revision": "f3e6b5fedbb52d1c4e04e3695b3127e6",
+    "url": "/static/media/image_part_008.f3e6b5fe.jpg"
+  },
+  {
+    "revision": "b06a556560385c20e8f3c9b774ea61db",
+    "url": "/index.html"
+  }
+];
